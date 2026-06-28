025b16c63e20c7b0ff6eeaef9252f09cf03b9b11
steve <stevenlarry1710@gmail.com>
2026-06-28 21:00:47 +0200

    edit start script on drone

--- start.bash
+++ start.bash
@@ -1,4 +1,4 @@
 source ./install/setup.bash
-ros2 run fcbridge && 
+ros2 run fcbridge bridge && 
 rpicam-vid -t 0   --low-latency   --inline   --width 1280 --height 720 --framerate 25   --codec h264 --profile baseline   --intra 25   --nopreview   -o "udp://192.168.0.103:50001?pkt_size=1316"
 
